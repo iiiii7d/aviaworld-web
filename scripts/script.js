b416a9dad43566a036c57bb3ebea6e16c6d40362
7d <i.second.7d@gmail.com>
2021-10-04 19:54:21 +0800

    pages now wont reload while navigating

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,55 @@
-document.addEventListener("DOMContentLoaded", yall);
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+/*function fadeOut(ele) {
+    ele.style.opacity = 1;
+    var fadeEffect = setInterval(() => {
+        if (ele.style.opacity > 0) ele.style.opacity -= 0.1;
+        else clearInterval(fadeEffect);
+    }, 20);
+}
+function fadeIn(ele) {
+    ele.style.opacity = 0;
+    var fadeEffect = setInterval(() => {
+        if (ele.style.opacity < 1) ele.style.opacity += 0.1;
+        else clearInterval(fadeEffect);
+    }, 20);
+}*/
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+window.addEventListener("popstate", e => {
+    //fadeOut(document.querySelector("main"));
+    if (e.state) $.get(e.state.href, data => {
+        let content = document.createElement("span");
+        content.innerHTML = data;
+        document.querySelector("main").innerHTML = content.querySelector("main").innerHTML;
+        content.remove();
+        //setTimeout(() => fadeIn(document.querySelector("main")), 1000)
+        setupPage();
+    });
+})
+
+function setupPage() {
+    yall();
+    document.querySelectorAll('a').forEach(anchor => {
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            if (anchor.getAttribute("href").startsWith("#")) {
+                console.log("a");
+                document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+            else if (!anchor.getAttribute("href").startsWith("http")) {
+                history.pushState({href: anchor.getAttribute("href")}, "", anchor.getAttribute("href"));
+                //fadeOut(document.querySelector("main"));
+                $.get(anchor.getAttribute("href"), data => {
+                    let content = document.createElement("span");
+                    content.innerHTML = data;
+                    document.querySelector("main").innerHTML = content.querySelector("main").innerHTML;
+                    content.remove();
+                    //setTimeout(() => fadeIn(document.querySelector("main")), 1000)
+                    setupPage();
+                });
+                return;
+            }
         });
     });
-});
+}
+setupPage();
