6f006c4ea533a44df9ee0256c7ffb8b46a65725d
7d <i.second.7d@gmail.com>
2021-10-06 19:59:10 +0800

    postpone ajax loading of pages to later

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ function fadeIn(ele) {
         if (ele.style.opacity < 1) ele.style.opacity += 0.1;
         else clearInterval(fadeEffect);
     }, 20);
-}*/
+}
 
 window.addEventListener("popstate", e => {
     //fadeOut(document.querySelector("main"));
@@ -23,21 +23,22 @@ window.addEventListener("popstate", e => {
         //setTimeout(() => fadeIn(document.querySelector("main")), 1000)
         setupPage();
     });
-})
+})*/
 
 function setupPage() {
     yall();
     document.querySelectorAll('a').forEach(anchor => {
         anchor.addEventListener('click', e => {
             let href = anchor.getAttribute("href")
-            e.preventDefault();
+            //e.preventDefault();
             if (href.startsWith("#")) {
+                e.preventDefault();
                 console.log("a");
                 document.querySelector(href).scrollIntoView({
                     behavior: 'smooth'
                 });
             }
-            else if (!href.startsWith("http")) {
+            /*else if (!href.startsWith("http")) {
                 history.pushState({href: href}, "", href);
                 //fadeOut(document.querySelector("main"));
                 $.get((href=="/") ? "index.html" : href, data => {
@@ -49,7 +50,7 @@ function setupPage() {
                     setupPage();
                 });
                 return;
-            }
+            }*/
         });
     });
 }
